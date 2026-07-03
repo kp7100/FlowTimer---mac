e9733c6a08314b1059125c2c5a511791198ce65e
Krishan Pareek <krishanpareek996@gmail.com>
2026-07-03 15:48:22 +0530

    Initial Commit

---
+++ FlowTimerTests/FlowTimerTests.swift
@@ -0,0 +1,19 @@
+//
+//  FlowTimerTests.swift
+//  FlowTimerTests
+//
+//  Created by Krishan Pareek on 7/3/26.
+//
+
+import Testing
+@testable import FlowTimer
+
+struct FlowTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
